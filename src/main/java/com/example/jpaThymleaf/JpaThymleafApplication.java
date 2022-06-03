d3c47fbc6305bd99865f2d560ae4eabcdbe0cd4b
jisu <jisu3316@naver.com>
2022-06-03 21:50:36 +0900

    jpaThymleaf

---
+++ src/main/java/com/example/jpaThymleaf/JpaThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaThymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaThymleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpaThymleaf/JpaThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaThymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
