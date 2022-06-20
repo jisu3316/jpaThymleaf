1b024ab468c06832ee78fbf81f5a8cd0178b77e3
jisu3316 <jisu3316@naver.com>
2022-06-20 23:02:58 +0900

    jpaThymleaf

--- src/main/java/com/example/jpaThymleaf/repository/UserRepository.java
+++ src/main/java/com/example/jpaThymleaf/repository/UserRepository.java
@@ -1,11 +1,22 @@
 package com.example.jpaThymleaf.repository;
 
 import com.example.jpaThymleaf.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import java.util.List;
 
 
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User>, CustomizedUserRepository {
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
+    @EntityGraph(attributePaths = { "boards" })
+    List<User> findAll();
     User findByUsername(String username);
+
+    @Query("select u from User u where u.username like %?1%")
+    List<User> findByUsernameQuery(String username);
+    @Query(value = "select * from User u where u.username like %?1%", nativeQuery = true)
+    List<User> findByUsernameNativeQuery(String username);
 }
